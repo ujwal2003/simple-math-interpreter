3ab8c2ecbede42a7d74482f5d459ef615805dfac
ujwal2003 <joshiujwal2003@gmail.com>
2022-12-23 16:25:52 -0600

    added tokens.h

--- math_intrptr.cpp
+++ math_intrptr.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "tokens.h"
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
---
+++ tokens.h
@@ -0,0 +1,28 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#include <iostream>
+
+using namespace std;
+
+enum TokenType {
+	T_Number,
+	T_Variable,
+	T_Equal,
+	T_Plus,
+	T_Minus,
+	T_Multiply,
+	T_Divide,
+};
+
+struct Token {
+	TokenType type;
+	string value;
+	
+	Token(TokenType inType, string inVal="") {
+		type = inType;
+		value = inVal;
+	}
+};
+
+#endif
