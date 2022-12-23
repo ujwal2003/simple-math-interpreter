3e2651afb53e40d9c351c7295985d38d9a390bf9
ujwal2003 <joshiujwal2003@gmail.com>
2022-12-23 16:06:06 -0600

    updated main cpp file
    updated startup message, fixed user input, and added comments

--- math_intrptr.cpp
+++ math_intrptr.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	
 	//startup message
 	cout << "simple c++ Math Interpreter." << endl;
-	cout << "Type 'exit()' and hit enter to close the program." << endl;
+	cout << "Type any accepted expression to evaluate or type \"exit()\" to close the program." << endl;
 	
 	//main input loop
 	bool continueInterpreter = true;
@@ -19,11 +19,12 @@ int main(int argc, char *argv[]) {
 		//get user input
 		string inputLine;
 		cout << ">>> ";
-		cin >> inputLine;
+		getline(cin, inputLine);
 		
 		if(inputLine != "exit()") {
 			cout << inputLine << endl; //temporary
 		} else {
+			//TODO: add "help()" message
 			//close the program when "exit()" is inputted
 			continueInterpreter = false;
 		}
