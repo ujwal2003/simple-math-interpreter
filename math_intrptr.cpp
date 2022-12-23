7ebc8d6e0d145b124e09293d0c41e10cf4cad3b2
ujwal2003 <joshiujwal2003@gmail.com>
2022-12-23 15:59:08 -0600

    main cpp file
    added startup message and comments

--- math_intrptr.cpp
+++ math_intrptr.cpp
@@ -9,9 +9,14 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	
-	//main loop
+	//startup message
+	cout << "simple c++ Math Interpreter." << endl;
+	cout << "Type 'exit()' and hit enter to close the program." << endl;
+	
+	//main input loop
 	bool continueInterpreter = true;
 	while(continueInterpreter) {
+		//get user input
 		string inputLine;
 		cout << ">>> ";
 		cin >> inputLine;
@@ -19,6 +24,7 @@ int main(int argc, char *argv[]) {
 		if(inputLine != "exit()") {
 			cout << inputLine << endl; //temporary
 		} else {
+			//close the program when "exit()" is inputted
 			continueInterpreter = false;
 		}
 	}
