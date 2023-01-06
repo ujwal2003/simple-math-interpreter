dc53be803c9a261ed87a6f7546dd9da2f372c3ef
ujwal2003 <joshiujwal2003@gmail.com>
2023-01-06 14:52:09 -0600

    unary operator node

--- ast.h
+++ ast.h
@@ -7,7 +7,7 @@ using namespace std;
 enum NodeType {
 	N_NULL, 
 	N_Number, N_Variable, 
-	N_Assign,
+	N_Assign, N_UnaryOp,
 	N_Add, N_Subtract, N_Multipy, N_Divide,
 	N_dummyOpenParen, N_dummyCloseParen
 };
@@ -24,6 +24,7 @@ class ASTNode {
 		void init_NumberNode(string number);
 		void init_VariableNode(string var);
 		void init_AssignNode(ASTNode* var, ASTNode* expression);
+		void init_UnaryOperator(string sign, ASTNode* atom); //atom will always be left node
 		void init_AddNode(ASTNode* opLeft, ASTNode* opRight);
 		void init_SubtractNode(ASTNode* opLeft, ASTNode* opRight);
 		void init_MultiplyNode(ASTNode* opLeft, ASTNode* opRight);
